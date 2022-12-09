17c5aa029e903a75f4bc550328d8e8ad5da995e7
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-09 18:47:22 +0700

    Defensive Programming

---
+++ Defensive_Programming.js
@@ -0,0 +1,20 @@
+function letterFinder(word, match) {
+    var condition1 = typeof(word) == 'string' && word.lenght >= 2;
+    var condition2 = typeof(match) == 'string' && word.lenght == 1;
+    if(condition1 && condition2){
+         for(i = 0; i < word.length; i++) {
+        if(word[i] == match) {
+            console.log('Found the', match, 'at', i)
+        } else {
+            console.log('---No match found at', i)
+        }
+    }
+
+    }
+    else{
+        console.log("Please pass correct arguments to the function")
+    }
+   
+}
+letterFinder([], [])
+letterFinder('cat', 'ct')
