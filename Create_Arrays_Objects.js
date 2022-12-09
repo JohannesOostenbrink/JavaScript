aa0f286defedd015c280ac37432998de2198a2c9
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-09 18:41:01 +0700

    Create Arrays and Objects

---
+++ Create_Arrays_Objects.js
@@ -0,0 +1,19 @@
+//Array of clothes
+//Adding and removing array items 
+
+var clothes = []
+clothes.push('shirt');
+clothes.push('pants');
+clothes.push('shoes');
+clothes.push('socks');
+clothes.push('boxer');
+clothes.pop();
+clothes.push('belt');
+console.log(clothes[2]);
+
+
+var favCar = {}
+favCar.color = 'blue';
+favCar.convertible = true;
+console.log(favCar)
+
