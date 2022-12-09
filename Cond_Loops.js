53d2b5d5fee9bb99d0c7447f114703a36ad36313
JohannesOostenbrink <oostenhan@gmail.com>
2022-12-09 18:30:30 +0700

    Working with conditionals and loops

---
+++ Cond_Loops.js
@@ -0,0 +1,30 @@
+// for with if statement 
+
+for (var i = 1; i <= 10; i++) {
+    if (i == 1) {console.log('Gold Metal')
+    }
+    else if (i == 2) {console.log('Silver Metal')
+    }
+    else if (i == 3) {console.log('Bronze Metal')
+    }
+    else {console.log(i)
+    }
+     }
+
+     //for with switch statement
+
+     for (var i = 1; i <= 10; i++) {
+        switch(i){
+            case 1:
+            console.log('Gold Metal')
+            break;
+            case 2: 
+            console.log('Silver Metal')
+            break;
+            case 3: 
+            console.log('Bronze Metal')
+            break;
+            default:
+            console.log(i)
+        }
+    }
